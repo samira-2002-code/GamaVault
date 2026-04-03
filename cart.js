590f5d52a466e710bc1b0d46291c11d43a2fc3a0
samira-2002-code <samiraassemlali2@gmail.com>
2026-04-03 15:50:05 +0100

    add cart item UI with quantity controls and remove button

--- cart.js
+++ cart.js
@@ -16,3 +16,29 @@ function renderCart() {
 
     cart.forEach((item, index) => {
         total += item.price * item.quantity;
+
+                const itemHtml = `
+            <div class="flex flex-col md:flex-row items-center justify-between bg-gray-800 p-4 rounded-xl gap-4">
+                <div class="flex items-center gap-4 w-full">
+                    <img src="${item.image}" class="w-20 h-20 object-cover rounded-lg">
+                    <div>
+                        <h3 class="font-bold text-lg">${item.title}</h3>
+                        <p class="text-green-400 font-semibold">${item.price} $</p>
+                    </div>
+                </div>
+                
+                <div class="flex items-center gap-6">
+                    <div class="flex items-center bg-gray-700 rounded-lg overflow-hidden">
+                        <button onclick="changeQty(${index}, -1)" class="px-3 py-1 ">-</button>
+                        <span class="px-4 font-bold text-blue-400">${item.quantity}</span>
+                        <button onclick="changeQty(${index}, 1)" class="px-3 py-1 ml-[5px] ">+</button>
+                    </div>
+                    
+                    <button onclick="removeItem(${index})" class="text-red-500 hover:text-red-400 text-xl">
+                       <img class="w-8 h-8 ml-10" src="trash.png" alt="trash"/>
+                    </button>
+                </div>
+            </div>
+        `;
+        cartItemsContainer.innerHTML += itemHtml;
+    });
