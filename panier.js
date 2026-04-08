6dba7e880e6ce2d23644668ae42fd14f81001648
samira-2002-code <samiraassemlali2@gmail.com>
2026-04-08 10:48:28 +0100

    add function show bascket

--- panier.js
+++ panier.js
@@ -1,76 +1,94 @@
-let cart = JSON.parse(localStorage.getItem('gamevault_cart')) || [];
+let panier = JSON.parse(localStorage.getItem('gamevault_cart')) || [];
 
-const cartItemsContainer = document.getElementById('cart-items');
-const cartTotalElement = document.getElementById('cart-total');
-const checkoutBtn = document.getElementById('checkout-btn');
+const container = document.getElementById('cart-items');
+const totalElement = document.getElementById('cart-total');
+const btnCommander = document.getElementById('checkout-btn');
 
-function renderCart() {
-    if (cart.length === 0) {
-        cartItemsContainer.innerHTML = '<p class="text-center text-gray-400 py-10">Votre panier est vide.</p>';
-        cartTotalElement.innerText = '0.00';
+// afficher panier
+function afficherPanier() {
+    if (panier.length === 0) {
+        container.innerHTML = '<p class="text-center text-gray-400 py-10">Votre panier est vide.</p>';
+        totalElement.innerText = "0.00";
         return;
     }
 
-    cartItemsContainer.innerHTML = '';
+    container.innerHTML = "";
     let total = 0;
 
-    cart.forEach((item, index) => {
+    for (let i = 0; i < panier.length; i++) {
+        let item = panier[i];
         total += item.price * item.quantity;
 
-        const itemHtml = `
-            <div class="flex flex-col md:flex-row items-center justify-between bg-gray-800 p-4 rounded-xl gap-4 mb-10">
-                <div class="flex items-center gap-4 w-full">
-                    <img src="${item.image}" class="w-20 h-20 object-cover rounded-lg">
-                    <div>
-                        <h3 class="font-bold text-lg">${item.title}</h3>
-                        <p class="text-green-400 font-semibold">${item.price} $</p>
-                    </div>
-                </div>
+        container.innerHTML += `
+        <div class="flex flex-col md:flex-row items-center justify-between bg-gray-800 p-4 rounded-xl gap-4 mb-10">
+            
+            <div class="flex items-center gap-4 w-full">
+                <img src="${item.image}" class="w-20 h-20 object-cover rounded-lg">
                 
-                <div class="flex items-center gap-6">
-                    <div class="flex items-center bg-gray-700 rounded-lg overflow-hidden">
-                        <button onclick="changeQty(${index}, -1)" class="px-3 py-4">-</button>
-                        <span class="px-4 font-bold text-blue-400">${item.quantity}</span>
-                        <button onclick="changeQty(${index}, 1)" class="px-8 py-4 ml-[5px]">+</button>
-                    </div>
-                    
-                    <button onclick="removeItem(${index})" class="text-red-500 text-xl">
-                        <img class="w-8 h-8 ml-10" src="/Image/trash.png" alt="trash"/>
-                    </button>
+                <div>
+                    <h3 class="font-bold text-lg">${item.title}</h3>
+                    <p class="text-green-400 font-semibold">${item.price} $</p>
                 </div>
             </div>
+            
+            <div class="flex items-center gap-4">
+                
+                <div class="flex items-center bg-gray-700 rounded-lg overflow-hidden">
+                    <button onclick="changer(${i}, -1)" class="px-3 py-4">-</button>
+                    <span class="px-4 font-bold text-blue-400">${item.quantity}</span>
+                    <button onclick="changer(${i}, 1)" class="px-8 py-4">+</button>
+                </div>
+                
+                <button onclick="supprimer(${i})" class="p-2  rounded-lg">
+                    <img class="w-6 h-6" src="./Image/trash.png" alt="trash">
+                </button>
+
+            </div>
+        </div>
         `;
+    }
 
-        cartItemsContainer.innerHTML += itemHtml;
-    });
-
-    cartTotalElement.innerText = total.toFixed(2);
+    totalElement.innerText = total.toFixed(2);
 }
 
-window.changeQty = (index, delta) => {
-    cart[index].quantity += delta;
-    if (cart[index].quantity < 1) cart[index].quantity = 1;
-    saveAndRender();
-};
+// changer quantité
+function changer(index, valeur) {
+    panier[index].quantity += valeur;
 
-window.removeItem = (index) => {
-    cart.splice(index, 1);
-    saveAndRender();
-};
+    if (panier[index].quantity < 1) {
+        panier[index].quantity = 1;
+    }
 
-function saveAndRender() {
-    localStorage.setItem('gamevault_cart', JSON.stringify(cart));
-    renderCart();
+    save();
 }
 
-checkoutBtn.addEventListener('click', () => {
-    if (cart.length === 0) return alert("votre panier est vide");
+// supprimer produit
+function supprimer(index) {
+    panier.splice(index, 1);
+    save();
+}
 
-    alert(" Commande réussie ");
-    cart = [];
-    saveAndRender();
-    window.location.href = 'index.html';
+// sauvegarder
+function save() {
+    localStorage.setItem('gamevault_cart', JSON.stringify(panier));
+    afficherPanier();
+}
+
+// commander
+btnCommander.addEventListener('click', function () {
+    if (panier.length === 0) {
+        alert("Votre panier est vide");
+        return;
+    }
+
+    alert("Commande réussie !");
+    panier = [];
+    save();
 });
 
+// rendre fonctions accessibles HTML
+window.changer = changer;
+window.supprimer = supprimer;
 
-renderCart();
+// start
+afficherPanier();    
